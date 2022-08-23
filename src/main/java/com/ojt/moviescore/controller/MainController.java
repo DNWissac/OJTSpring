272515a6e715345f5d810ae90fc11923c2741958
DNWissac <issac1162@danawa.com>
2022-08-24 08:51:10 +0900

    220823

---
+++ src/main/java/com/ojt/moviescore/controller/MainController.java
@@ -0,0 +1,60 @@
+package com.ojt.moviescore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 메인 컨트롤러
+ * 페이지 이동 등을 담당하는 컨트롤러
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String mainView() {
+        return "index";
+    }
+
+    @RequestMapping("/test")
+    public String getMessage(Model model) {
+        model.addAttribute("testStr", "타임리프 연습");
+        return "testView";
+    }
+
+    /**
+     * 회원가입 페이지로 이동
+     * @return String 회원가입 폼으로 이동
+     */
+    @RequestMapping("/signupform")
+    public String signupForm() {
+        return "views/signupform";
+    }
+
+    /**
+     * 로그인 페이지로 이동
+     * @return String 로그인 페이지로 이동
+     */
+    @RequestMapping("/signinform")
+    public String signinForm() {
+        return "views/signinform";
+    }
+
+    /**
+     * 영화 등록 페이지로 이동
+     * @return String 영화 등록 페이지로 이동
+     */
+    @RequestMapping("/movieinsertform")
+    public String movieinsertForm() {
+        return "views/movieinsertform";
+    }
+
+    /**
+     * 영화 수정 페이지로 이동
+     * @return String 영화 수정 페이지로 이동
+     */
+    @RequestMapping("/movieupdateform")
+    public String movieupdateForm() {
+        return "views/movieupdateform";
+    }
+}
---
+++ src/main/java/com/ojt/moviescore/model/dto/MovieDto.java
@@ -0,0 +1,21 @@
+package com.ojt.moviescore.model.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MovieDto {
+
+    private Integer nMovieSeq;              // 영화 번호 (PK)
+    private String sMovieTitle;             // 영화 제목
+    private String sMovieStory;             // 영화 내용
+    private String sMovieImage;             // 영화 사진 경로
+    private String dtOpeningDate;           // 영화 개봉일자
+    private String dtMovieDate;             // 영화 등록일자
+    private String sMovieDirector;          // 영화 감독 이름
+    private String sGenreID;                // 영화 장르 ID (FK)
+
+
+}
